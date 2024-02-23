d56a35c178000c905118a9b1b46cc97283079b05
Rocco João <joaogabrielrs25@gmail.com>
2024-02-22 22:21:34 -0300

    criacao do spring boot 22/02

---
+++ src/main/java/com/teste/fatec/fatecteste/FatecTesteApplication.java
@@ -0,0 +1,13 @@
+package com.teste.fatec.fatecteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/fatec/fatecteste/FatecTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.fatec.fatecteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatecTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
