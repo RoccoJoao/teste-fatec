7f1770374c06153c113d236ed591724a6fd4552a
RoccoJoao <joaogabrielrs25@gmail.com>
2024-02-29 22:41:12 -0300

    43110

--- src/main/java/com/teste/fatec/fatecteste/FatecTesteApplication.java
+++ src/main/java/com/teste/fatec/fatecteste/FatecTesteApplication.java
@@ -2,10 +2,27 @@ package com.teste.fatec.fatecteste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.websocket.server.PathParam;
+
+@RestController
 @SpringBootApplication
 public class FatecTesteApplication {
-
+	@RequestMapping("/")
+	String home() {
+		return "hello world";
+	}
+	@RequestMapping("/number")
+	int home3() {
+		return 110;
+	}
+	@RequestMapping("/number1/{num}")
+	Integer home2(@PathVariable Integer num) {
+		return num;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FatecTesteApplication.class, args);
 	}
