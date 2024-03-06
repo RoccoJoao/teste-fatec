1d12796739f660004e6a2dbfd070d1bd48064afa
RoccoJoao <joaogabrielrs25@gmail.com>
2024-03-06 19:26:46 -0300

    classificar

--- src/main/java/com/teste/fatec/fatecteste/FatecTesteApplication.java
+++ src/main/java/com/teste/fatec/fatecteste/FatecTesteApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.websocket.server.PathParam;
 
 @RestController
@@ -23,6 +22,21 @@ public class FatecTesteApplication {
 	Integer home2(@PathVariable Integer num) {
 		return num;
 	}
+    @RequestMapping("/classificar/{idade}")
+    String classificarIdade(@PathVariable int idade) {
+        if (idade < 0) {
+            return "Idade inválida";
+        } else if (idade < 12) {
+            return "Criança";
+        } else if (idade >= 12 && idade <= 18) {
+            return "Adolescente";
+        } else if (idade >= 19 && idade <= 60) {
+            return "Adulto";
+        } else {
+            return "Idoso";
+        }
+    }
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(FatecTesteApplication.class, args);
 	}
