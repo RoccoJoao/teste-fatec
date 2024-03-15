012c9a36725c72cbaf27529a1fffcd4366059667
RoccoJoao <joaogabrielrs25@gmail.com>
2024-03-14 22:50:03 -0300

    Cliente_14_03_2.0

--- src/main/java/com/teste/fatec/fatecteste/entities/Cliente.java
+++ src/main/java/com/teste/fatec/fatecteste/entities/Cliente.java
@@ -1,6 +1,5 @@
 package com.teste.fatec.fatecteste.entities;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +14,46 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	@Column(nullable = false, length = 60 )
 	private String nome;
 	
