1ca5f4d92fded099c2a5d0116548c1e6251c6c5a
RoccoJoao <joaogabrielrs25@gmail.com>
2024-05-08 20:50:39 -0300

    dia 08

--- src/main/java/br/com/api_fatec/api_fatec/domain/cliente/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.api_fatec.api_fatec.domain.cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api_fatec.api_fatec.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
